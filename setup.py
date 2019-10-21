9cc918e71049369f6dec262589e896b17b45520a
Gunnink <pietergunnink@gmail.com>
2019-10-21 12:46:14 +0200

    Added setup.py for pip install -e  support

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='magnons',
+      version='0.1',
+      description='Magnon dispersion and other useful things',
+      author='Pieter Gunnink',
+      author_email='pietergunnink@gmail.com',
+      packages=['magnons'])
