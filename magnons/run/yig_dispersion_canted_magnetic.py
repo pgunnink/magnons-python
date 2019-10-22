930afe52d8ed3d116f6b2bb2376b3ac16b02eeea
Gunnink <pietergunnink@gmail.com>
2019-10-22 17:34:07 +0200

    Now saving data

--- magnons/run/yig_dispersion_canted_magnetic.py
+++ magnons/run/yig_dispersion_canted_magnetic.py
@@ -4,7 +4,9 @@ from magnons.yig import a, S, mu, J, Ms
 from magnons.cgs import E_to_GHz
 import matplotlib.pyplot as plt
 from magnons.magnetization import magnetization_angle
-
+from magnons.data import Data
+from copy import copy
+import os
 if __name__ == '__main__':
     N = 400
     H = 2500
@@ -20,27 +22,42 @@ if __name__ == '__main__':
         "Ng": 4,
     }
     theta = 0
-
+    save_dir = './dispersion/canted_field/'
+    os.makedirs(save_dir, exist_ok=True)
+    save_dir_h5 = './data/'
+    os.makedirs(save_dir_h5, exist_ok=True)
+    save_file = save_dir_h5 + 'main_data.h5'
     kwargs["N"] = N
-    firstN = 30
+    firstN = 10
     alpha = [0, 30, 60, 80]
+    plt.figure(dpi=200, figsize=(16, 8))
     for i, al in enumerate(alpha):
         al = np.radians(al)
         phi = magnetization_angle(al, M=Ms, H=H)
-        res, kvalues = get_dispersion_theta(theta,
-                                            12,
-                                            ky_begin=10**4,
-                                            ky_end=2 * 10**6,
-                                            phi=phi,
-                                            firstN=firstN,
-                                            use_angled_if_zero=True,
-                                            logspace=False,
-                                            **kwargs)
+        res, ev, kvalues = get_dispersion_theta(theta,
+                                                48,
+                                                ky_begin=10**2,
+                                                ky_end=2 * 10**6,
+                                                phi=phi,
+                                                firstN=firstN,
+                                                use_angled_if_zero=True,
+                                                logspace=False,
+                                                return_eigenfunctions=True,
+                                                **kwargs)
+
         absk = np.sqrt(np.sum(kvalues**2, axis=1))
+        with Data(save_file) as f:
+            save_kwargs = copy(kwargs)
+            del save_kwargs["h"]
+            save_kwargs["H"] = H
+            save_kwargs["alpha"] = np.degrees(al)
+            save_kwargs["phi"] = np.degrees(phi)
+            save_kwargs["theta"] = np.degrees(theta)
+            f.save_data(kvalues, res, ev, **save_kwargs)
         plt.subplot(2, 2, i + 1)
         for i in range(firstN):
             plt.plot(absk, res[:, i], "-", color="black")
         plt.title(
             f'H angle {np.round(np.degrees(al))}, M angle {np.round(np.degrees(phi))}'
         )
-    plt.show()
+    plt.savefig(save_dir + 'canted_magnetic_field.png')
